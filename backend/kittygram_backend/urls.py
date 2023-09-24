c1745221c0ed8cfa0ee4d553dcf1e401facb4005
Ushanov-Ilya <jkaterinov@gmail.com>
2023-09-24 12:08:04 +0000

    test

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
